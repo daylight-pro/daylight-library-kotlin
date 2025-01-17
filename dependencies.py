dcdd1fe80fd9262e9db98f13508e9c420372d6bc
Daylight <33833751+daylight-pro@users.noreply.github.com>
2025-01-17 21:19:44 +0900

    依存追加

---
+++ dependencies.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+argv = os.sys.argv
+
+with open(argv[1], 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        if line.startswith('import '):
+            print(line.split(' ')[1].replace('.', '/')[0:-1] + '.kt')
